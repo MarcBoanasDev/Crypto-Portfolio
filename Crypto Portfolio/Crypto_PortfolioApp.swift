161ae0a8c21f0818d21b3b5849df9608f61f368d
Marc Boanas <marcjohnboanas@gmail.com>
2022-10-10 12:25:57 +0100

    Initial Commit

---
+++ Crypto Portfolio/Crypto_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_PortfolioApp.swift
+//  Crypto Portfolio
+//
+//  Created by Marc Boanas on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
